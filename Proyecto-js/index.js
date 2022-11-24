45413f4a769d1cc94ba2f21bf0e3fbc89d903adb
Kenneth <kentag94@gmail.com>
2022-11-23 21:28:33 -0600

    protecto-js

---
+++ Proyecto-js/index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + "muy feliz"
+    console.log(b)
+}
+
+fn()
+
+const clickeando = () => {
+    let a = "chanchito feliz"
+    
+    console.log(a)
+}
